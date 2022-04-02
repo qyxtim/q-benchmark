d3f5d77dfd2db16574cf722cbefa120ae05c0b0d
Tim Qiu <qyxtim@qq.com>
2022-04-02 20:46:21 +0100

    Improve timing accuracy

--- timer.hpp
+++ timer.hpp
@@ -10,8 +10,8 @@ public:
     double stop()
     {
         auto m_end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(m_end - m_start);
-        return duration.count();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(m_end - m_start);
+        return duration.count() * 0.001;
     }
 
 private:
